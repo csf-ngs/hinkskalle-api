b647b72048f81009dbe2d74bb913470ec7997efe
Heinz Axelsson-Ekker <heinz.ekker@vbcf.ac.at>
2021-06-22 16:56:27 +0200

    fix tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,8 +8,8 @@ from hinkskalle_api import HinkApi
 
 class TestApi(unittest.TestCase):
   def test_init(self):
-    api = HinkApi(base='http://testha.se', key='secret')
-    self.assertEqual(api.base, 'http://testha.se/')
+    api = HinkApi(base='http://testha.se/', key='secret')
+    self.assertEqual(api.base, 'http://testha.se')
   
   def test_config_file(self):
     with tempfile.TemporaryDirectory() as tmp_dir, mock.patch('hinkskalle_api.api.os.path.expanduser') as mock_exp:
@@ -17,7 +17,7 @@ class TestApi(unittest.TestCase):
       with open(mock_exp(), 'w') as tmpfh:
         tmpfh.write("hink_api_base: http://testha.se/\nhink_api_key: secret\n")
       api = HinkApi()
-      self.assertEqual(api.base, 'http://testha.se/')
+      self.assertEqual(api.base, 'http://testha.se')
       self.assertEqual(api.key, 'secret')
       mock_exp.assert_called_with('~/.hink_api.yml')
 
@@ -29,12 +29,14 @@ class TestApi(unittest.TestCase):
   def test_config_env(self):
     with mock.patch.dict(os.environ, { 'HINK_API_BASE': 'http://testha.se', 'HINK_API_KEY': 'secret'}):
       api = HinkApi()
-      self.assertEqual(api.base, 'http://testha.se/')
+      self.assertEqual(api.base, 'http://testha.se')
       self.assertEqual(api.key, 'secret')
     
   def test_no_config(self):
-    with self.assertRaisesRegex(FileNotFoundError, r'/\.hink_api.yml'):
-      api = HinkApi()
+    with tempfile.TemporaryDirectory() as tmp_dir, mock.patch('hinkskalle_api.api.os.path.expanduser') as mock_exp:
+      mock_exp.return_value = os.path.join(tmp_dir, 'test.yml')
+      with self.assertRaisesRegex(FileNotFoundError, r'/test\.yml'):
+        api = HinkApi()
 
     with tempfile.TemporaryDirectory() as tmp_dir, mock.patch('hinkskalle_api.api.os.path.expanduser') as mock_exp:
       mock_exp.return_value = os.path.join(tmp_dir, 'test.yml')
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -13,9 +13,8 @@ class TestCli(unittest.TestCase):
     def test_help(self):
         """Test the CLI."""
         runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'hinkskalle_api.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
+        result = runner.invoke(cli.cli)
+        self.assertEqual(result.exit_code, 0)
+        help_result = runner.invoke(cli.cli, ['--help'])
+        self.assertEqual(help_result.exit_code, 0)
+        self.assertRegexpMatches(help_result.output, r'--help\s+Show this message and exit.')
