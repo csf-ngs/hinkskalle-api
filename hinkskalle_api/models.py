2824420ca7165ffe238d071475fc42e3387a2828
Heinz Axelsson-Ekker <heinz.ekker@vbcf.ac.at>
2021-07-06 10:35:47 +0200

    switch to dataclass for models

--- hinkskalle_api/models.py
+++ hinkskalle_api/models.py
@@ -1,12 +1,13 @@
 from humanize import naturalsize
+from dataclasses import dataclass, field
 import typing
 
+@dataclass
 class BaseModel:
-  def __init__(self, name: str, description: str, size: int = 0, usedQuota: int = 0):
-    self.name = name
-    self.description = description
-    self.size = size
-    self.usedQuota = usedQuota
+  name: str 
+  description: str
+  size: int = 0
+  usedQuota: int = 0
 
   def __str__(self):
     return f"""- name: {self.name}
@@ -15,47 +16,47 @@ class BaseModel:
   usedQuota: {naturalsize(self.usedQuota)}
 """
 
+@dataclass
 class User:
-  def __init__(self, username: str, is_admin: bool):
-    self.username = username
-    self.is_admin = is_admin
+  username: str
+  is_admin: bool
 
+@dataclass
 class Entity(BaseModel):
-  def __init__(self, **kwargs):
-    super().__init__(**kwargs)
+  pass
 
+@dataclass
 class Collection(BaseModel):
-  def __init__(self, **kwargs):
-    super().__init__(**kwargs)
+  pass
 
+@dataclass
 class Container(BaseModel):
-  def __init__(self, type: str, **kwargs):
-    super().__init__(**kwargs)
-    self.type=type
+  type: typing.Optional[str] = None
 
   def __str__(self):
     out = super().__str__()
     out += f"  type: {self.type}\n"
     return out
 
+@dataclass
 class Tag:
-  def __init__(self, name: str, arch: str = None):
-    self.name=name
-    self.arch=arch
+  name: str
+  arch: typing.Optional[str] = None
   def __str__(self):
     return f"""- name: {self.name}
   arch: {self.arch}
 """
 
+
+@dataclass
 class Manifest:
-  def __init__(self, id: int, hash: str, filename: str, type: str, total_size: str, tags: typing.List[Tag], image_hash=None):
-    self.id = id
-    self.hash = hash
-    self.filename = filename
-    self.type = type
-    self.total_size = total_size
-    self.tags = tags
-    self.image_hash = image_hash
+  id: int
+  hash: str
+  filename: str
+  type: str
+  total_size: str
+  tags: typing.List[Tag] = field(default_factory=list)
+  image_hash: typing.Optional[str] = None
 
   def __str__(self):
     return f"""- filename: {self.filename}
